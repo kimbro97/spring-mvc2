bd604aa6b171e3f5faecdd7ac0e27c41c69d8c7e
kimbro97 <rlagudwog@gmail.com>
2024-06-16 12:22:01 +0900

    feat: 템플릿 레이아웃

--- thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/TemplateController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/TemplateController.java
@@ -3,12 +3,22 @@ package hello.thymeleafbasic.basic;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Component
 @Controller
+@RequestMapping("/template")
 public class TemplateController {
     @GetMapping("fragment")
     public String template() {
         return "template/fragment/fragmentMain";
+    }
+    @GetMapping("layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
+    @GetMapping("/layoutExtend")
+    public String layoutExtend() {
+        return "template/layoutExtend/layoutExtendMain";
     }
 }
